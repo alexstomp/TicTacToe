b0487a55953f53a9577a0f464613b0c5cf1cbfc7
Alex Stomp <stompy1208@gmail.com>
2012-04-02 01:27:05 -0500

    one exception minimax

--- lib/computer.rb
+++ lib/computer.rb
@@ -9,44 +9,89 @@ class Computer < Player
     @turn = turn
   end
 
- def get_move(board)
+  def get_move(board)
     space_values = {}
-    board.open_spaces.each do |origin_space|
-      space_values[origin_space] = branch(board, origin_space).to_f
+
+    finish = finishing_move(board, @turn)
+    if finish == false
+      board.open_spaces.each do |open_space|
+        move_value = get_branch(board, open_space)
+        space_values[open_space] = (move_value*100).round / 100.0
+      end
+      pick_value(space_values)
+    else
+      finish
     end
-    puts space_values
-    pick_value(space_values)
   end
 
-  def branch(board, space, depth=1, value=0)
-    gen_board = Board.new(board.side)
+  def get_branch(board, space, depth=1, value=0)
+
+    player = @turn
+    opponent = player == 1 ? 2 : 1
+
+    gen_board = Board.new(3)
     board.board.each_with_index {|value, index| gen_board.board[index] = value}
     gen_board.game_state
 
-    player_value = (board.p1_board.size > board.p2_board.size) ? "O" : "X"
+    if player == 1
+      player_value = (depth%2) == 0 ? "O" : "X"
+    elsif opponent == 1
+      player_value = (depth%2) == 0 ? "X" : "O"
+    end
 
     gen_board.move(space, player_value)
 
-    if gen_board.game_state == :incomplete
+    win = finishing_move(gen_board, player)
+    loss = finishing_move(gen_board, opponent)
+
+    if win != false
+      value += Computer.assign_value(depth)
+    elsif loss != false
+      value += Computer.assign_value(depth, false)
+    elsif gen_board.game_state == :incomplete
       gen_board.open_spaces.each do |next_space|
-        value = branch(gen_board, next_space, depth+1, value) if depth <= 3
+        depth += 1
+        value += get_branch(gen_board, next_space, depth)
       end
-    else
-      value += assign_value(gen_board, depth)
     end
     value
   end
 
-  def assign_value(finished_board, depth)
-    if finished_board.game_state == :p1_win and @turn == 1
-      1.fdiv(depth)
-    elsif finished_board.game_state == :p2_win and @turn == 2
-      1.fdiv(depth)
-    elsif finished_board.game_state == :draw
-      0
+  def finishing_move(board, player)
+
+    if player == 1
+      player_board = board.p1_board
     else
-      -1.fdiv(depth)
+      player_board = board.p2_board
     end
+
+    board.winning_sets.each do |winning_set|
+      combo_spaces = 0
+      combo = []
+      winning_set.to_a.each do |set_space|
+        if player_board.include?(set_space)
+          combo_spaces += 1
+          combo << set_space
+        end
+      end
+      if combo_spaces == 2
+        winning_space = winning_set.to_a
+        combo.each do |combo_space|
+          winning_space.delete(combo_space)
+        end
+        return winning_space[0] if board.open_spaces.include?(winning_space[0])
+      end
+    end
+    return false
+  end
+
+  def self.assign_value(depth, win=true)
+    if win
+      val = 1.fdiv(depth*depth)
+    else
+      val = -1.fdiv(depth*depth)
+    end
+    return val
   end
 
   def pick_value(hash, pick=nil)
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -24,20 +24,86 @@ end
 
 describe Computer do
 
-  it "can_win returns winning move if can win" do
-    player = MockComputer.new(1)
-    board = Board.new(3)
-    board.board = ["X", "X", " ", "O", "O", " ", " ", " ", " "]
-    board.game_state
-    player.can_win(board, player.turn).should == 2
+  before(:each) do
+    @player = MockComputer.new(2)
+    @board = Board.new(3)
   end
 
-  it "can_win false if can't win" do
-    player = MockComputer.new(1)
-    board = Board.new(3)
-    board.board = ["X", " ", " ", "O", " ", " ", " ", " ", " "]
-    board.game_state
-    player.can_win(board, player.turn).should == false
+  it "finishing_move function returns winning move if can win" do
+    @board.board = ["X", "X", " ", "O", "O", " ", " ", " ", " "]
+    @board.game_state
+    @player.finishing_move(@board, @player.turn).should == 5
+  end
+
+  it "finishing_move function doesn't return taken spaces" do
+    @board.board = ["X", " ", " ", "O", "O", " ", "X", " ", " "]
+    @board.game_state
+    @player.finishing_move(@board, @player.turn).should_not == 3
+  end
+
+  it "finishing_move function false if can't be finished" do
+    @board.board = ["X", " ", " ", "O", " ", " ", " ", " ", " "]
+    @board.game_state
+    @player.finishing_move(@board, @player.turn).should == false
+  end
+
+  it "picks highest valued key in hash" do
+    hash = {0 => 1, 1 => 2, 2 => 3}
+    @player.pick_value(hash).should == 2
+  end
+
+  it "makes a winning move" do
+    @board.board = ["O", "O", " ", "X", "X", " ", " ", " ", " "]
+    @board.game_state
+    @player.get_move(@board).should == 2
+  end
+
+  it "works 1" do
+    @board.board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+    @board.game_state
+    @player.get_move(@board).should == 4
+  end
+
+  it "works 2" do
+    @board.board = [" ", " ", "X", " ", " ", " ", " ", " ", " "]
+    @board.game_state
+    @player.get_move(@board).should == 4
+  end
+
+  it "works 3" do
+    @board.board = [" ", " ", " ", " ", " ", " ", " ", " ", "X"]
+    @board.game_state
+    @player.get_move(@board).should == 4
+  end
+
+  it "works 4" do
+    @board.board = [" ", " ", " ", " ", " ", " ", "X", " ", " "]
+    @board.game_state
+    @player.get_move(@board).should == 4
+  end
+
+
+  it "makes a blocking move" do
+    @board.board = ["X", " ", " ", "O", "O", " ", "X", " ", " "]
+    @board.game_state
+    @player.get_move(@board).should == 5
+  end
+
+  it "makes a blocking move" do
+    @player = MockComputer.new(2)
+    @board.board = ["X", " ", " ", " ", " ", " ", "X", " ", "O"]
+    @board.game_state
+    @player.get_move(@board).should == 3
+  end
+
+  it "assigns value correctly with depth for loss" do
+    pending
+    MockComputer.assign_value(4, false).should == -5.fdiv(4*4)
+  end
+
+  it "assigns value correctly with depth for win" do
+    pending
+    MockComputer.assign_value(5).should == 2.fdiv(5*5)
   end
 
 end
@@ -65,21 +131,69 @@ end
 
 class MockComputer < MockPlayer
 
-  attr_reader :turn, :space_choices
+  attr_reader :turn
 
   def initialize(turn)
     @turn = turn
   end
 
   def get_move(board)
+    space_values = {}
+
+    finish = finishing_move(board, @turn)
+    if finish == false
+      board.open_spaces.each do |open_space|
+        move_value = get_branch(board, open_space)
+        space_values[open_space] = (move_value*100).round / 100.0
+      end
+      puts "\n" + space_values.to_s
+      pick_value(space_values)
+    else
+      finish
+    end
   end
 
-  def can_win(board, player)
+  def get_branch(board, space, depth=1, value=0)
+
+    player = @turn
+    opponent = player == 1 ? 2 : 1
+
+    gen_board = Board.new(3)
+    board.board.each_with_index {|value, index| gen_board.board[index] = value}
+    gen_board.game_state
+
+    if player == 1
+      player_value = (depth%2) == 0 ? "O" : "X"
+    elsif opponent == 1
+      player_value = (depth%2) == 0 ? "X" : "O"
+    end
+
+    gen_board.move(space, player_value)
+
+    win = finishing_move(gen_board, player)
+    loss = finishing_move(gen_board, opponent)
+
+    if win != false
+      value += MockComputer.assign_value(depth)
+    elsif loss != false
+      value += MockComputer.assign_value(depth, false)
+    elsif gen_board.game_state == :incomplete
+      gen_board.open_spaces.each do |next_space|
+        depth += 1
+        value += get_branch(gen_board, next_space, depth)
+      end
+    end
+    value
+  end
+
+  def finishing_move(board, player)
+
     if player == 1
       player_board = board.p1_board
     else
       player_board = board.p2_board
     end
+
     board.winning_sets.each do |winning_set|
       combo_spaces = 0
       combo = []
@@ -94,10 +208,26 @@ class MockComputer < MockPlayer
         combo.each do |combo_space|
           winning_space.delete(combo_space)
         end
-        return winning_space[0]
+        return winning_space[0] if board.open_spaces.include?(winning_space[0])
       end
     end
     return false
+  end
+
+  def self.assign_value(depth, win=true)
+    if win
+      val = 1.fdiv(depth*depth)
+    else
+      val = -1.fdiv(depth*depth)
+    end
+    return val
+  end
+
+  def pick_value(hash, pick=nil)
+    hash.each do |key, value|
+      pick = key if pick == nil or value >= hash[pick]
+    end
+    pick
   end
 
 end
